d22bfa4ace30df6b7ed321152ac22099c58e8893
Coralie Collignon <coralie.collignon@gmail.com>
2017-08-10 09:33:30 +0200

    first refacto

--- encryption_engine.rb
+++ encryption_engine.rb
@@ -1,48 +1,57 @@
 class EncryptionEngine
 
   def encrypt(string, key)
-    array_capitals = string.split("").map { |letter| capitalized?(letter) }
     message_splitted = string.downcase.split("")
-    message_splitted.map! do |letter|
+    en = encryptor(message_splitted, key)
+    final_string_with_capitals(en, string)
+  end
+
+  def decrypt(string, key)
+    message_splitted = string.split("")
+    de = decryptor(message_splitted, key)
+    final_string_with_capitals(de, string)
+  end
+
+  private
+
+  def letter_capitalized?(letter)
+    letter == letter.upcase
+  end
+
+  def string_capitalized?(string)
+    string.split("").map { |letter| letter_capitalized?(letter) }
+  end
+
+  def encryptor(array, key)
+    array.map! do |letter|
       if letter.ord + key > "z".ord
-        new_number = letter.ord + key - 1 - "z".ord + "a".ord
+        new_number = letter.ord + key - "z".ord + "a".ord - 1
       else
         new_number = letter.ord + key
       end
       new_number = 32 if new_number == 45
       new_number.chr
     end
+  end
 
+  def decryptor(array, key)
+    array.map! do |letter|
+      if letter.ord - key < "a".ord
+        new_number = letter.ord - key + "z".ord - "a".ord + 1
+      else
+        new_number = letter.ord - key
+      end
+      new_number = 32 if new_number == 45
+      new_number.chr
+    end
+  end
+
+  def final_string_with_capitals(array, string)
     result = []
-    message_splitted.each_with_index do |letter, index|
-      letter.upcase! if array_capitals[index]
+    array.each_with_index do |letter, index|
+      letter.upcase! if string_capitalized?(string)[index]
       result << letter
     end
     result.join
   end
-
-  def capitalized?(string)
-    string.split("").map { |letter| letter == letter.upcase }
-  end
-
-  def capitalize_array(array)
-    result = []
-    array.each_with_index do |letter, index|
-      letter.upcase! if capitalized?[index]
-      result << letter
-    end
-  end
-
-  def decrypt(string, key)
-    # Your code here
-
-    message_splitted = string.split("")
-    message_splitted.map! do |letter|
-      new_number = letter.ord - key
-      new_number.chr
-    end
-    message_splitted.join
-  end
-
-
 end
